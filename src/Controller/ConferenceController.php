ad90bae084081e3cc052ca1dc63372b6393e8997
Abdourahmane DIOP <abdourahmane.diop@digeezy.com>
2021-12-28 14:59:32 +0100

    6.4 : Adding an Easter Egg

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,21 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
     {
         /*return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);*/
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s !</h1>', htmlspecialchars($name));
+        }
         return new Response(
             <<<EOF
 <html>
     <body>
+        $greet
         <img src="/images/under-construction.gif" />
     </body>
 </html>
