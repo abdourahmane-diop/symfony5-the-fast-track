4b2544aea918957b09f807958e512143993b7b7c
Abdourahmane DIOP <abdourahmane.diop@digeezy.com>
2022-01-02 18:36:23 +0100

    10.3 : Using Twig in a controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,32 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        /*return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);*/
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s !</h1>', htmlspecialchars($name));
-        }
-        return new Response(
-            <<<EOF
-<html>
-    <body>
-        $greet
-        <img src="/images/under-construction.gif" />
-    </body>
-</html>
-EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
